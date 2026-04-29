095b94a4215a5b9b693fe413e96982f3e29fa4de
Gabriel <gabrielnegreirossaraiva38@gmail.com>
2026-04-29 14:38:22 -0300

    chore: script vapid

---
+++ scripts/gen_vapid.py
@@ -0,0 +1,15 @@
+from py_vapid import Vapid
+import base64
+
+v = Vapid()
+v.generate_keys()
+priv_pem = v.private_pem()
+s = __import__("cryptography.hazmat.primitives.serialization", fromlist=["x"])
+pub_b64 = base64.urlsafe_b64encode(
+    v._private_key.public_key().public_bytes(
+        s.Encoding.X962,
+        s.PublicFormat.UncompressedPoint
+    )
+).rstrip(b"=").decode()
+open("/opt/caloria/vapid_private.pem", "wb").write(priv_pem)
+print("VAPID_PUBLIC_KEY=" + pub_b64)
